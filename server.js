9362f93e29bbcdf1651b14f24189f4bdc383c22d
Arny <arne.n.bjerkaas@gmail.com>
2020-01-02 18:36:54 +0100

    Add routes

--- server.js
+++ server.js
@@ -11,12 +11,20 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
+
+const exercisesRouter = require('./src/routes/exercises');
+const usersRouter = require('./src/routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter)
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}, yey!`);
