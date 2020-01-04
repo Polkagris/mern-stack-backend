0dad222c50980c2998128c337cfa75714d78be6b
Arny <arne.n.bjerkaas@gmail.com>
2020-01-04 13:27:06 +0100

    Add CRUD to user model

--- src/routes/users.js
+++ src/routes/users.js
@@ -22,4 +22,33 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json("Error:" + err))
 });
 
+router.route('/:id').get(async (req, res) => {
+    try {
+        const userById = await User.findById(req.params.id);
+        return res.json(userById);
+    } catch (err) {
+        res.status(400).json("Error: ", err);
+    }
+});
+
+router.route('/update/:id').post(async (req, res) => {
+    try {
+        const userToBeUpdated = await User.findById(req.params.id);
+        userToBeUpdated.username = req.body.username;
+        await userToBeUpdated.save();
+        return res.json("User updated: ", userToBeUpdated);
+    } catch (err) {
+        res.status(400).json("Error: ", err);
+    }
+});
+
+router.route('/:id').delete(async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        return res.json("User deleted!");
+    } catch (err) {
+        res.status(400).json("Error: ", err);
+    }
+});
+
 module.exports = router;
