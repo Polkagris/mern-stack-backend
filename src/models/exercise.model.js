e617e53b9d6addd5a0daac1d74b724ff46dca78b
unknown <arne.n.bjerkaas@gmail.com>
2020-03-04 21:03:20 +0100

    Add duration to schema

--- src/models/exercise.model.js
+++ src/models/exercise.model.js
@@ -1,20 +1,24 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({
+const exerciseSchema = new Schema(
+  {
     description: { type: String, required: true },
+    duration: { type: String },
+    date: { type: Date },
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
     }
     // username: { type: String, required: true },
     // duration: { type: Number, required: true },
     // date: { type: Date, required: false },
+  },
+  {
+    timestamps: true
+  }
+);
 
-}, {
-        timestamps: true,
-    });
-
-const Exercise = mongoose.model('Exercise', exerciseSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 module.exports = Exercise;
